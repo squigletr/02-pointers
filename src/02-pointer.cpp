da1e666f5d47b14393b60545aff27eda1e3b6616
Cristopher Ceja <squiggleletter@proton.me>
2024-02-15 11:50:13 -0600

    Hacer conteo en memoria dinamica

--- src/02-pointer.cpp
+++ src/02-pointer.cpp
@@ -2,21 +2,21 @@
 
 template <typename T> class SmartPointer {
 	T* ptr = nullptr;
-	int numRef = 0;
+	int* numRef;
 public:
 	T* getPtr() const {
 		return ptr;
 	}
-	int getRef() const {
+	int* getRef() const {
 		return numRef;
 	}
 	void incRef(){
-		numRef++;
-		std::cout<<"Incremented " << ptr << " usage to " << numRef << "\n";
+		(*numRef)++;
+		std::cout<<"Incremented " << ptr << " usage to " << *numRef << "\n";
 	}
 	void decRef(){
-		numRef--;
-		std::cout<<"Decremented " << ptr << " usage to " << numRef << "\n";
+		(*numRef)--;
+		std::cout<<"Decremented " << ptr << " usage to " << *numRef << "\n";
 	}
 	SmartPointer(SmartPointer<T>& value){
 		this->numRef = value.getRef();
@@ -25,27 +25,33 @@ public:
 	}
 	SmartPointer(T* value){
 		ptr = value;
+		numRef = new int(0);
 		incRef();
 	}
 	~SmartPointer(){
 		decRef();
-		if(numRef == 0 && ptr != nullptr){
-			std::cout<<"Freeing mem " << ptr << ", was " << *ptr << "\n";
+		if(*numRef == 0){
+			std::cout<<" Freeing mem " << ptr << ", was " << *ptr << "\n";
 			delete ptr;
+			delete numRef;
 			ptr = nullptr;
 		}
 	}
 
 	SmartPointer<T>& operator=(SmartPointer<T>& value){
-		if(getRef() == 1){
+		// Reassign to itself, no op
+		if(&value == this)
+			return *this;
+
+		if(*getRef() == 1){
 			std::cout<<"Reassigning " << ptr << " to " << value.getPtr() << "\n"
 				<< " And freeing mem " << ptr << ", was " << *ptr << "\n";
 			delete ptr;
+			delete numRef;
 
 			ptr = value.getPtr();
 			value.incRef();
 			numRef = value.getRef();
-			decRef();
 		} else {
 			// Dunno how to deal with dangling pointers
 			std::cout<<"Can't reassign because pointer was shared\n"
@@ -58,11 +64,18 @@ public:
 };
 
 int main(){
+	std::cout<< "### Starting program ###" << std::endl;
+	std::cout<< std::endl << "### Outer scope ###" << std::endl;
 	SmartPointer<int> num(new int(4));
+	std::cout<< std::endl << "### Inner scope ###" << std::endl;
 	{
+		num = num;
 		// Uncomment to test dangling pointer
-		//SmartPointer<int> num2 = num;
+		SmartPointer<int> num2 = num;
 		SmartPointer<int> num3(new int(10));
-		num = num3;
+		//num = num3;
+		std::cout<< std::endl << "### Outer scope ###" << std::endl;
 	}
+	std::cout<< std::endl << "### Exiting program ###" << std::endl;
+	return 0;
 }
